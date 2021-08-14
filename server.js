dbadeb644b33a16f32fc16e32034fff02e32c746
Deidrah <dominikastasik93@gmail.com>
2021-08-14 12:24:33 +0200

    add sockets and the communication between, also small refactor

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const socket = require('socket.io');
 const cors = require('cors');
 const app = express();
-
 app.use(cors());
 
 let tasks = [];
@@ -13,7 +12,25 @@ app.use((req,res) => {
   });
 });
 
-const server = app.listen(8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
+
 const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! His id is ' + socket.id);
+  socket.emit('updateTasks', tasks);
+  socket.on('removeTask', (id) => {
+    tasks = tasks.filter(task => task.id !== id);
+    socket.broadcast.emit('removeTask', id);
+  });
+  socket.on('addTask', ({id, name}) => {
+    const newTask = {
+      id,
+      name,
+    };
+    tasks = [...tasks, newTask];
+    socket.broadcast.emit('addTask', newTask);
+  });
+});
